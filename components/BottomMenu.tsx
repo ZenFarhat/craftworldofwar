db9ba1afd594442516f7035b8fca1008bd8c9612
makkiveli <ahmed.alkhazragi01@gmail.com>
2022-04-29 20:47:49 +1000

    added coin.svg removed bag button

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -4,7 +4,6 @@ const BottomMenu = () => {
   return (
     <div className={bottomMenuStyles.bottom__menu}>
       <div className={bottomMenuStyles.bottom__menuicon}>B</div>
-      <button className="Bag">Bag</button>
     </div>
   )
 }
